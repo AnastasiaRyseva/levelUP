1632941d54ec64fdb5c1c53b4112f22a261c99fb
AnastasiaRyseva <92371518+AnastasiaRyseva@users.noreply.github.com>
2023-01-24 17:46:29 +0300

    add new block

--- js/index.js
+++ js/index.js
@@ -1,37 +1,61 @@
-const video = document.getElementById("bgVideo");
-const btnPause = document.getElementById("myBtn");
+
+//слайдер
+new Swiper('.review-swiper', {
+  navigation: {
+    nextEl: '.next2',
+    prevEl: '.prev2'
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 40
+    },
+    1010: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    }
+  }
+});
+
+//ТАЙМЕР
+let sec = 1800;
+let countDiv = document.getElementById("mv_timer");
+let countDown = setInterval(function () {
+    secpass();
+  }, 1000);
+
+function secpass() {
+    let min = Math.floor(sec / 60),
+        remSec = sec % 60;
     
-function pauseVideo() {
-    if(video.paused) {
-        video.play();
-        btnPause.innerHTML = "Pause";
+    if(remSec < 10) {
+        remSec = '0' + remSec;
+    }
+    if (min < 10) {
+      min = '0' + min;
+    }
+    countDiv.innerHTML = min + ":" + remSec;
+    if(sec > 0) {
+        sec = sec - 1;
     } else {
-        video.pause();
-        btnPause.innerHTML = "Play";
+        clearInterval(countDown);
+        countDiv.innerHTML = 'countdown done';
     }
 }
 
-new Swiper('.review-swiper', {
-    navigation: {
-		nextEl: '.next2',
-		prevEl: '.prev2'
-	},
-    breakpoints: {
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 20
-        },
-        480: {
-          slidesPerView: 1,
-          spaceBetween: 30
-        },
-        640: {
-          slidesPerView: 1,
-          spaceBetween: 40
-        },
-        1010: {
-          slidesPerView: 2,
-          spaceBetween: 20
-        }
-    }
-});
+// скролл к форме обратного звонка
+
+const forCall = document.querySelectorAll('.forCall');
+const callForm = document.querySelector('.callback');
+
+forCall.forEach(item => item.onclick = function() {
+    callForm.scrollIntoView({block: 'center',  behavior: 'smooth'});
+})
